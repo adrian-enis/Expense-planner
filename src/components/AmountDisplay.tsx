6b4bb22d3b52d1edf8925b11b541986f992ba89f
Adrian Enis <adrianenis2000@gmail.com>
2025-04-27 18:33:12 -0400

    commit

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -1,11 +1,15 @@
 import { formatCurrency } from "../helpers"
 
 
+
+/*
+@params
+ */
 export type AmountDisplayProps = {
     label?: string
     amount:number
 }
-
+//El componente AmoundDisplay, muestra el texto donde se mostrara el monto(Budget:$00, Avalable:$00, Spent:$00)
 const AmountDisplay = ({label, amount}:AmountDisplayProps) => {
   return (
     <p className="text-2xl text-blue-600 font-bold">
